3537ee6be84f1166f0ecaba10cc89d63cd241553
Pelumi Amao <113313095+Pekumiiix@users.noreply.github.com>
2024-12-14 21:26:28 +0100

    Update header.tsx

--- app/(landing-page)/sections/header.tsx
+++ app/(landing-page)/sections/header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="w-full h-fit flex justify-center max-w-[1536px] bg-hero-bg bg-center bg-cover">
-      <nav className="container py-8 px-5 lg:px-32 flex items-center justify-between z-10 bg-red-900">
+      <nav className="container py-8 px-5 lg:px-32 flex items-center justify-between z-10">
         <div className="flex items-center px-1 gap-2">
           {/* <Image src={``} alt="Logo" width={28} height={40} /> */}
           <p className="text-foreground font-semibold">Chronicles</p>
